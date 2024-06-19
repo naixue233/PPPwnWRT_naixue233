89a3cda2f0507b9b68a8d8befbcc3d838e21a612
naixue233 <122972585+naixue233@users.noreply.github.com>
2024-06-19 18:41:59 +0800

    Update MIPS.sh

--- scripts/MIPS.sh
+++ scripts/MIPS.sh
@@ -4,8 +4,8 @@
 opkg update
 opkg install unzip coreutils-rm libpcap1
 cd /tmp
-wget https://github.com/FalsePhilosopher/PPPwnWRT/raw/main/zips/files.zip
-wget https://github.com/FalsePhilosopher/PPPwnWRT/raw/main/zips/MIPS.zip
+wget https://github.com/naixue233/PPPwnWRT_naixue233/blob/main/zips/files.zip
+wget https://github.com/naixue233/PPPwnWRT_naixue233/blob/main/zips/MIPS.zip
 unzip -d / files.zip
 unzip -d / MIPS.zip
 rm files.zip MIPS.zip
